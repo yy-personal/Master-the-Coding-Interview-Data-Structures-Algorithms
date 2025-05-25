7b7ea7fd2fa17f21bbcb5109e4e1a5e5b093f3ec
yy <30777685+yy-personal@users.noreply.github.com>
2025-05-26 00:22:50 +0800

    update

--- ztm-master-the-coding-interview-ds-algo-main/01_arrays/arrays.js
+++ ztm-master-the-coding-interview-ds-algo-main/01_arrays/arrays.js
@@ -3,18 +3,17 @@ const numbers = [1,2,3,4,5];
 // Variable array is somewhere in memory and the computer knows it.
 // When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
 
-
 //push
-strings.push('e');
+strings.push('e'); // O(1)
 
 //pop
 strings.pop();
-strings.pop();
+strings.pop(); // O(1)
 
 //unshift
-strings.unshift('x')
+strings.unshift('x') // O(n)
 
 //splice
-strings.splice(2, 0, 'alien');
+strings.splice(2, 0, 'alien'); // O(n)
 
 console.log(strings)

b4617f639659b9f37b34542aad2f0fece918a00e
yy <30777685+yy-personal@users.noreply.github.com>
2025-06-04 23:59:29 +0800

    Update firstRecurringCharacterExercise.js

--- ztm-master-the-coding-interview-ds-algo-main/02_hashTables/firstRecurringCharacterExercise.js
+++ ztm-master-the-coding-interview-ds-algo-main/02_hashTables/firstRecurringCharacterExercise.js
@@ -9,9 +9,24 @@
 //It should return undefined
 
 
-function firstRecurringCharacter(input) {}
+function firstRecurringCharacter(input) {
+    let obj = {}
+    for (let index = 0; index < input.length; index++) {
+        // console.log(input[index]);
+		if (!obj[input[index]]) {
+			obj[input[index]] = 1;
+		} else {
+			obj[input[index]] = obj[input[index]] + 1;
+		}
 
+        if (obj[input[index]] > 1) {
+            return input[index];
+		}
+	}
+    return undefined
+}
 
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]
 // return 5 because the pairs are before 2,2
